fc1c00335c8bc8df5212708e9a5fded806fb19c3
Jon Malis <jmalis@live.com>
2015-08-24 09:34:07 -0700

    bloccit crud intro assignment

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,8 +1,15 @@
 class PostsController < ApplicationController
+
   def index
-    #declare an instance variable @posts and assign it a collection of Post objects using the all method, provided by ActiveRecord.
-    #all returns a collection of Post objects.
-    @posts = Post.all
+  #declare an instance variable @posts and assign it a collection of Post objects using the all method, provided by ActiveRecord.
+  #all returns a collection of Post objects.
+  @posts = Post.all
+  @posts.each_with_index do |post, index| 
+  if index % 5 == 4 # since index starts at 0, every 5th object will be at positions 4, 9, 14, 19 etc.
+    # Do the change on post object
+    post.update_attributes(title: 'CENSORED')
+  end
+end
   end
 
   def show
