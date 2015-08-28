1569b5d923b4fc41bdb93ff8571f2b18d067adf2
Jon Malis <jmalis@live.com>
2015-08-27 17:41:43 -0700

    bloccit validations checkpoint

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  #define a method named form_group_tag which takes two arguments.
+  #The first argument is an array of errors, and the second is a block.
+  def form_group_tag(errors, &block)
+  # if the errors array is not empty, the content_tag helper method is called.
+    if errors.any?
+      content_tag :div, capture(&block), class: 'form-group has-error'
+    else
+      content_tag :div, capture(&block), class: 'form-group'
+    end
+  end
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -9,6 +9,15 @@ RSpec.describe Post, type: :model do
 
   it {should belong_to(:topic)}
 
+  #test that Post validates the presence of title, body, and topic.
+  it { should validate_presence_of(:title) }
+  it { should validate_presence_of(:body) }
+  it { should validate_presence_of(:topic) }
+  #test that Post validates the lengths of title and body.
+  it { should validate_length_of(:title).is_at_least(5) }
+  it { should validate_length_of(:body).is_at_least(20) }
+
+
    context "attributes" do
      #test whether post has an attribute named title. This tests whether post will return a non-nil value when post.title is called.
      it "should respond to title" do
